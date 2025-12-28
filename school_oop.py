f15a6ae58b254e3486b8ed1e823fdef52fa0783f
theHrDev <hamzatrukayat2822@gmail.com>
2025-12-28 21:02:43 +0100

    Added the class structure for the school DB

---
+++ school_oop.py
@@ -0,0 +1,26 @@
+class Students:
+    def __init__(self):
+        pass
+    
+    def add_student(self):
+        pass
+    
+    def delete_student(self):
+        pass
+    
+    def edit_student(self):
+        pass
+    
+    
+    def view_single_student(self):
+        pass
+    
+    
+    def view_all_students(self):
+        pass
+    
+class Student:
+    def __init__(self,name,dept,age):
+        self.name = name
+        self.dept = dept
+        self.age = age
